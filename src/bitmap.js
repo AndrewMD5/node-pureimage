4b234fb9b20be458f36273708ef61a78f2e3462d
andrew <hi@andrew.im>
2022-09-17 22:15:01 +0200

    cloudflare worker changes

--- src/bitmap.js
+++ src/bitmap.js
@@ -1,6 +1,6 @@
-import {NAMED_COLORS} from "./named_colors.js"
-import {Context} from "./context.js"
-import {fromBytesBigEndian, getBytesBigEndian} from './uint32.js'
+import { NAMED_COLORS } from "./named_colors.js"
+import { Context } from "./context.js"
+import { fromBytesBigEndian, getBytesBigEndian } from './uint32.js'
 
 /**
  * The Bitmap class is used for direct pixel manipulation(for example setting a pixel colour,
@@ -18,7 +18,7 @@ export class Bitmap {
      * @param {any}   options Currently unused
      * @memberof Bitmap
      */
-    constructor(w,h, options) {
+    constructor(w, h, options) {
 
         /**
          * @type {number}
@@ -33,10 +33,10 @@ export class Bitmap {
         /**
          * @type {ArrayBuffer}
          */
-        this.data = Buffer.alloc(w*h*4);
+        this.data = new Uint8Array(w * h * 4);
 
         const fillval = NAMED_COLORS.transparent
-        for(let j=0; j<h; j++) {
+        for (let j = 0; j < h; j++) {
             for (let i = 0; i < w; i++) {
                 this.setPixelRGBA(i, j, fillval);
             }
@@ -54,11 +54,11 @@ export class Bitmap {
      *
      * @memberof Bitmap
      */
-    calculateIndex (x,y) {
+    calculateIndex(x, y) {
         x = Math.floor(x);
         y = Math.floor(y);
-        if (x<0 || y<0 || x >= this.width || y >= this.height) return 0;
-        return (this.width*y+x)*4;
+        if (x < 0 || y < 0 || x >= this.width || y >= this.height) return 0;
+        return (this.width * y + x) * 4;
     }
 
     /**
@@ -72,13 +72,13 @@ export class Bitmap {
      *
      * @memberof Bitmap
      */
-    setPixelRGBA(x,y,rgba) {
+    setPixelRGBA(x, y, rgba) {
         let i = this.calculateIndex(x, y);
         const bytes = getBytesBigEndian(rgba);
-        this.data[i+0] = bytes[0];
-        this.data[i+1] = bytes[1];
-        this.data[i+2] = bytes[2];
-        this.data[i+3] = bytes[3];
+        this.data[i + 0] = bytes[0];
+        this.data[i + 1] = bytes[1];
+        this.data[i + 2] = bytes[2];
+        this.data[i + 3] = bytes[3];
     }
 
     /**
@@ -95,12 +95,12 @@ export class Bitmap {
      *
      * @memberof Bitmap
      */
-    setPixelRGBA_i(x,y,r,g,b,a) {
+    setPixelRGBA_i(x, y, r, g, b, a) {
         let i = this.calculateIndex(x, y);
-        this.data[i+0] = r;
-        this.data[i+1] = g;
-        this.data[i+2] = b;
-        this.data[i+3] = a;
+        this.data[i + 0] = r;
+        this.data[i + 1] = g;
+        this.data[i + 2] = b;
+        this.data[i + 3] = a;
     }
 
     /**
@@ -113,13 +113,13 @@ export class Bitmap {
      *
      * @memberof Bitmap
      */
-    getPixelRGBA(x,y) {
+    getPixelRGBA(x, y) {
         let i = this.calculateIndex(x, y);
         return fromBytesBigEndian(
-            this.data[i+0],
-            this.data[i+1],
-            this.data[i+2],
-            this.data[i+3]);
+            this.data[i + 0],
+            this.data[i + 1],
+            this.data[i + 2],
+            this.data[i + 3]);
     }
 
     /**
@@ -134,9 +134,9 @@ export class Bitmap {
      *
      * @memberof Bitmap
      */
-    getPixelRGBA_separate(x,y) {
+    getPixelRGBA_separate(x, y) {
         const i = this.calculateIndex(x, y)
-        return this.data.slice(i,i+4);
+        return this.data.slice(i, i + 4);
     }
 
     /**
@@ -150,27 +150,27 @@ export class Bitmap {
         return new Context(this);
     }
 
-    _copySubBitmap(x,y,w,h) {
-        let dst = new Bitmap(w,h,{})
-        for(let i=0; i<w; i++) {
-            for(let j=0; j<h; j++) {
-                let indexA = this.calculateIndex(x+i,y+j)
-                let indexB = dst.calculateIndex(i,j)
-                for(let k=0; k<4; k++) {
-                    dst.data[indexB+k] = this.data[indexA+k]
+    _copySubBitmap(x, y, w, h) {
+        let dst = new Bitmap(w, h, {})
+        for (let i = 0; i < w; i++) {
+            for (let j = 0; j < h; j++) {
+                let indexA = this.calculateIndex(x + i, y + j)
+                let indexB = dst.calculateIndex(i, j)
+                for (let k = 0; k < 4; k++) {
+                    dst.data[indexB + k] = this.data[indexA + k]
                 }
             }
         }
         return dst
     }
 
-    _pasteSubBitmap(src,x,y) {
-        for(let i=0; i<src.width; i++) {
-            for(let j=0; j<src.height; j++) {
-                let indexA = this.calculateIndex(x+i,y+j)
-                let indexB = src.calculateIndex(i,j)
-                for(let k=0; k<4; k++) {
-                    this.data[indexA+k] = src.data[indexB+k]
+    _pasteSubBitmap(src, x, y) {
+        for (let i = 0; i < src.width; i++) {
+            for (let j = 0; j < src.height; j++) {
+                let indexA = this.calculateIndex(x + i, y + j)
+                let indexB = src.calculateIndex(i, j)
+                for (let k = 0; k < 4; k++) {
+                    this.data[indexA + k] = src.data[indexB + k]
                 }
             }
         }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import {Bitmap} from './bitmap.js'
-import {PNG} from "pngjs"
+import { Bitmap } from './bitmap.js'
+import { PNG } from "pngjs/browser"
 import * as JPEG from "jpeg-js"
-import {getBytesBigEndian} from './uint32.js'
+import { getBytesBigEndian } from './uint32.js'
 
 export * from './text.js'
 
-export function make (w,h,options) {
-    return new Bitmap(w,h,options)
+export function make(w, h, options) {
+    return new Bitmap(w, h, options)
 }
 
 /**
@@ -18,8 +18,8 @@ export function make (w,h,options) {
  * @returns {Promise<void>}
  */
 export function encodePNGToStream(bitmap, outstream) {
-    return new Promise((res,rej)=>{
-        if(!bitmap.hasOwnProperty('data') || !bitmap.hasOwnProperty('width') || !bitmap.hasOwnProperty('height')) {
+    return new Promise((res, rej) => {
+        if (!bitmap.hasOwnProperty('data') || !bitmap.hasOwnProperty('width') || !bitmap.hasOwnProperty('height')) {
             return rej(new TypeError('Invalid bitmap image provided'));
         }
 
@@ -28,13 +28,13 @@ export function encodePNGToStream(bitmap, outstream) {
             height: bitmap.height
         })
 
-        for(let i=0; i<bitmap.width; i++) {
-            for(let j=0; j<bitmap.height; j++) {
+        for (let i = 0; i < bitmap.width; i++) {
+            for (let j = 0; j < bitmap.height; j++) {
                 const rgba = bitmap.getPixelRGBA(i, j)
                 const n = (j * bitmap.width + i) * 4
                 const bytes = getBytesBigEndian(rgba)
-                for(let k=0; k<4; k++) {
-                    png.data[n+k] = bytes[k];
+                for (let k = 0; k < 4; k++) {
+                    png.data[n + k] = bytes[k];
                 }
             }
         }
@@ -59,17 +59,17 @@ export function encodePNGToStream(bitmap, outstream) {
  * @returns {Promise<Bitmap>}
  */
 export function decodePNGFromStream(instream) {
-    return new Promise((res,rej)=>{
+    return new Promise((res, rej) => {
         instream.pipe(new PNG())
-            .on("parsed", function() {
-                const bitmap = new Bitmap(this.width, this.height,{})
-                for(let i=0; i<bitmap.data.length; i++) {
+            .on("parsed", function () {
+                const bitmap = new Bitmap(this.width, this.height, {})
+                for (let i = 0; i < bitmap.data.length; i++) {
                     bitmap.data[i] = this.data[i];
                 }
                 res(bitmap);
-            }).on("error", function(err) {
-            rej(err);
-        });
+            }).on("error", function (err) {
+                rej(err);
+            });
     })
 };
 
@@ -86,8 +86,8 @@ export function decodePNGFromStream(instream) {
  */
 export function encodeJPEGToStream(img, outstream, quality) {
     quality = quality || 90;
-    return new Promise((res,rej)=> {
-        if(!img.hasOwnProperty('data') || !img.hasOwnProperty('width') || !img.hasOwnProperty('height')) {
+    return new Promise((res, rej) => {
+        if (!img.hasOwnProperty('data') || !img.hasOwnProperty('width') || !img.hasOwnProperty('height')) {
             return rej(new TypeError('Invalid bitmap image provided'));
         }
         const data = {
@@ -113,20 +113,34 @@ export function encodeJPEGToStream(img, outstream, quality) {
  * @returns {Promise<Bitmap>}
  */
 export function decodeJPEGFromStream(data) {
-    return new Promise((res,rej)=>{
+    return new Promise((res, rej) => {
         try {
             const chunks = []
             data.on('data', chunk => chunks.push(chunk));
-            data.on('end',() => {
-                const buf = Buffer.concat(chunks)
+            data.on('end', () => {
+
+                // Get the total length of all arrays.
+                let length = 0;
+                chunks.forEach(item => {
+                    length += item.length;
+                });
+
+                // Create a new array with total length and merge all source arrays.
+                const buf = new Uint8Array(length);
+                let offset = 0;
+                chunks.forEach(item => {
+                    buf.set(item, offset);
+                    offset += item.length;
+                });
+
                 let rawImageData = null
                 try {
                     rawImageData = JPEG.decode(buf);
-                } catch(err) {
+                } catch (err) {
                     rej(err);
                     return
                 }
-                const bitmap = new Bitmap(rawImageData.width, rawImageData.height,{})
+                const bitmap = new Bitmap(rawImageData.width, rawImageData.height, {})
                 for (let x_axis = 0; x_axis < rawImageData.width; x_axis++) {
                     for (let y_axis = 0; y_axis < rawImageData.height; y_axis++) {
                         const n = (y_axis * rawImageData.width + x_axis) * 4
